f6f1ea06d577cc1d193d1b9be059a00ea9b444d0
vivek <vivek5411nkbps@gmail.com>
2026-08-05 10:38:40 +0530

    new javascript file

---
+++ f1/f2/f3/a.js
@@ -0,0 +1,15 @@
+// Simple JavaScript code
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
+
+// Simple arithmetic
+const sum = (a, b) => a + b;
+console.log(sum(5, 3)); // Output: 8
+
+// Simple array operation
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled); // Output: [2, 4, 6, 8, 10]
